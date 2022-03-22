70d2a8e569348938101c21f566eba1867e5c72bb
DESKTOP-3SDFCG5\sayak <sayaka.uu.3838@gmail.com>
2022-03-23 06:48:41 +0900

    jQueryを追加

---
+++ index.js
@@ -0,0 +1,13 @@
+jQuery(".p-sidebar__title").click(function () {
+  jQuery(".l-main__body").toggleClass("is-open");
+  jQuery(".p-sidebar__back").toggleClass("is-open");
+  jQuery(".l-main__right").toggleClass("is-open");
+  jQuery(".p-sidebar__button").toggleClass("is-open");
+});
+
+jQuery(".p-sidebar__button").click(function () {
+  jQuery(".l-main__body").toggleClass("is-open");
+  jQuery(".p-sidebar__back").toggleClass("is-open");
+  jQuery(".l-main__right").toggleClass("is-open");
+  jQuery(".p-sidebar__button").toggleClass("is-open");
+});
