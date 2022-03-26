52edfcfc17c0b0999b11de710d4e09490544529b
DESKTOP-3SDFCG5\sayak <sayaka.uu.3838@gmail.com>
2022-03-26 15:55:48 +0900

    ヘッダー検索フォームの修正、ヘッダーｈｔｍｌ修正

--- js/index.js
+++ js/index.js
@@ -1,31 +1,13 @@
-// //Menuボタンをクリックすれば、クラスis-openを付与し、サイドバー展開
-// $('#menu_btn').on('click', function(){
-//   SlideMenu()
-// });
-
-// // スライドメニュー関数
-// const SlideMenu = () => {
-//   if($('#menu').hasClass("SlideOut")){
-//     // スライドメニューが非表示なら表示
-//     $('#menu').removeClass('SlideOut');
-//     $('#menu').addClass('SlideIN');
-//   } else {
-//     // スライドメニューがあれば非表示
-//     $('#menu').removeClass('SlideIN');
-//     $('#menu').addClass('SlideOut');
-//   }
-// }
-
 jQuery("#menu_btn").click(function () {
   jQuery(".l-main__body").toggleClass("is-open");
+  jQuery(".l-main__back").toggleClass("is-open");
   jQuery(".l-main__right").toggleClass("is-open");
-  jQuery(".p-sidebar__back").toggleClass("is-open");
   jQuery(".p-sidebar__button").toggleClass("is-open");
 });
 //×ボタンをクリックすれば、クラスis-openを削除し、サイドバー展開
 jQuery("#close_btn").click(function () {
   jQuery(".l-main__body").toggleClass("is-open");
+  jQuery(".l-main__back").toggleClass("is-open");
   jQuery(".l-main__right").toggleClass("is-open");
-  jQuery(".p-sidebar__back").toggleClass("is-open");
   jQuery(".p-sidebar__button").toggleClass("is-open");
 });
