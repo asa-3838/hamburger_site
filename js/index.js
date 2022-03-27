120b9bfd0a4206ff790bf11a72ed15ff71938d68
DESKTOP-3SDFCG5\sayak <sayaka.uu.3838@gmail.com>
2022-03-27 16:30:51 +0900

    クローン作成、元リポジトリを削除

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
+//Menuをクリックすれば、クラスis-openを追加し、サイドバーが開く
 jQuery("#menu_btn").click(function () {
   jQuery(".l-main__body").toggleClass("is-open");
   jQuery(".l-main__back").toggleClass("is-open");
   jQuery(".l-main__right").toggleClass("is-open");
   jQuery(".p-sidebar__button").toggleClass("is-open");
 });
-//×ボタンをクリックすれば、クラスis-openを削除し、サイドバー展開
+//×ボタンをクリックすれば、クラスis-openを削除し、サイドバーが閉じる
 jQuery("#close_btn").click(function () {
   jQuery(".l-main__body").toggleClass("is-open");
   jQuery(".l-main__back").toggleClass("is-open");
